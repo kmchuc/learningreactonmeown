8fc23ddec7957b5b93f71db8728a297d45b6bbcc
Kimberly Chuc <kimmchuc@gmail.com>
2020-07-26 18:43:17 -0700

    created Hearder, Inventory, and Order components for shop site

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,12 +4,11 @@ class StorePicker extends React.Component {
   render() {
     return(
       <form className="store-selector">
-        { /* comment */ }
         <h2>Please Enter A Store: </h2>
         <input type="text" required placeholder="Store Name" />
         <button type='submit'>Visit Store -></button>
       </form>
-    )
+    );
     }
 }
 
